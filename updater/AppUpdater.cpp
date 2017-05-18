f03598c18655797e16361ed3232777327034053b
Ben Kuper <bkuperberg@gmail.com>
2017-05-18 18:51:09 +0200

    updater fix invalid json file

--- updater/AppUpdater.cpp
+++ updater/AppUpdater.cpp
@@ -34,25 +34,30 @@ void AppUpdater::checkForUpdates()
 	{
 		String content = stream->readEntireStreamAsString();
 		var data = JSON::parse(content);
-		String version = data.getProperty("version", "");
-		bool beta = data.getProperty("beta", false);
-		Array<var> * changelog = data.getProperty("changelog", var()).getArray();
-		
-		if (Engine::mainEngine->checkFileVersion(data.getDynamicObject(), true))
-		{
-			String msg = "A new " + String(beta ? "beta " : "") + "version of Chataigne is available : " + version + "\nChangelog :\n";
-			
-			for (auto &c : *changelog) msg += c.toString() + "\n";
-			msg += "Do you want to download it ?";
 
-			int result = AlertWindow::showOkCancelBox(AlertWindow::InfoIcon, "New version available", msg, "Yes", "No");
-			if (result) downloadURL.launchInDefaultBrowser();
-		}
-		else
+		if (data.isObject())
 		{
-			LOG("App is up to date.");
+			if (Engine::mainEngine->checkFileVersion(data.getDynamicObject(), true))
+			{
+				String version = data.getProperty("version", "");
+				bool beta = data.getProperty("beta", false);
+				Array<var> * changelog = data.getProperty("changelog", var()).getArray();
+				
+				String msg = "A new " + String(beta ? "beta " : "") + "version of Chataigne is available : " + version + "\n\nChangelog :\n";
+
+				for (auto &c : *changelog) msg += c.toString() + "\n";
+				msg += "\nDo you want to go to the download page ?";
+
+				int result = AlertWindow::showOkCancelBox(AlertWindow::InfoIcon, "New version available", msg, "Yes", "No");
+				if (result) downloadURL.launchInDefaultBrowser();
+			} else
+			{
+				LOG("App is up to date.");
+			}
+		} else
+		{
+			LOG("Error, update file is not valid");
 		}
 		
-
 	}	
 }
