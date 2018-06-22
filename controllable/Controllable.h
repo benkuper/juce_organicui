2ddd2f169798c30e36142a591239e26ac5a14f5e
Ben Kuper <bkuperberg@gmail.com>
2018-06-22 12:25:09 +0200

    fix controllable templated function

--- controllable/Controllable.h
+++ controllable/Controllable.h
@@ -185,11 +185,30 @@ public:
 		bool undo() override;
 	};
 
-
-
-
 	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(Controllable)
 };
+
+
+//Helpers
+template<class T>
+T * Controllable::findParentAs(int maxLevel)
+{
+	int curLevel = 0;
+	if (parentContainer == nullptr) return nullptr;
+
+	auto * cc = this->parentContainer;
+	T * result = dynamic_cast<T *>(cc);
+
+	while (result == nullptr && cc != nullptr)
+	{
+		cc = cc->parentContainer;
+		result = dynamic_cast<T *>(cc);
+		curLevel++;
+		if (maxLevel != -1 && curLevel > maxLevel) return nullptr;
+	}
+
+	return result;
+}
 
 
 
