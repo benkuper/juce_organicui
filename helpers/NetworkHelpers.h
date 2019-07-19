a63b3c37235a3d1ebc403e50edc6f3fbbe0d093c
Ben Kuper <bkuperberg@gmail.com>
2019-07-19 21:06:16 +0200

    added 192.168.3

--- helpers/NetworkHelpers.h
+++ helpers/NetworkHelpers.h
@@ -9,7 +9,7 @@ public:
 		for (auto &ip : ips)
 		{
 			if (ip.startsWith("192.168.0.") || ip.startsWith("192.168.1.") || ip.startsWith("192.168.2.")) return ip;
-			if (ip.startsWith("192.168.4.") || ip.startsWith("192.168.43.")) return ip;
+			if (ip.startsWith("192.168.3.") ||  ip.startsWith("192.168.4.") || ip.startsWith("192.168.43.")) return ip;
 		}
         
         return "";	
@@ -22,7 +22,7 @@ public:
 		IPAddress::findAllAddresses(ad);
 		for (auto &ip : ad)
 		{
-			if (ip.toString().startsWith("192.168.0.") || ip.toString().startsWith("192.168.1.") || ip.toString().startsWith("192.168.4.") || ip.toString().startsWith("192.168.43."))
+			if (ip.toString().startsWith("192.168.0.") || ip.toString().startsWith("192.168.1.") || ip.toString().startsWith("192.168.2.") || ip.toString().startsWith("192.168.3.") || ip.toString().startsWith("192.168.4.") || ip.toString().startsWith("192.168.43."))
 			{
 				result.add(ip.toString());
 			}
