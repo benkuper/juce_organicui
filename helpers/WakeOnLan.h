ce16df4ba9746e02fa96683d2a8627804ee4a8c3
Ben Kuper <bkuperberg@gmail.com>
2020-07-21 16:22:06 +0200

    wakeOnLan update

--- helpers/WakeOnLan.h
+++ helpers/WakeOnLan.h
@@ -13,30 +13,5 @@
 class WakeOnLan
 {
 public:
-	static void wake(MACAddress * macAddress)
-	{
-		
-		uint8 ffBytes[6] = { 0xFF,0xFF,0xFF,0xFF,0xFF,0xFF };
-		const uint8 *macBytes = macAddress->getBytes();
-		
-		const int packetSize = 6 + 16 * 6;
-		uint8 packet[packetSize];
-		
-		for (int i = 0; i < 6; ++i) packet[i] = ffBytes[i];
-		
-		for (int i = 0; i < 16; ++i)
-		{
-			for (int j = 0; j < 6; ++j)
-			{
-				int index = 6 + i * 6 + j;
-				packet[index] = macBytes[j];
-			}
-		}
-		
-		DatagramSocket s;
-		s.write(IPAddress::broadcast().toString(),9,packet,packetSize);
-
-		//LOG("Wake ON Lan sent to " + macAddress->toString());
-	}
-
+    static int wake(MACAddress* macAddress);
 };
