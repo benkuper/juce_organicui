9650896b3fc7f09266b2a24b1a8b3ba77089b8db
Ben Kuper <bkuperberg@gmail.com>
2020-07-21 16:24:59 +0200

    fix f*g WakeOnLan

--- helpers/WakeOnLan.cpp
+++ helpers/WakeOnLan.cpp
@@ -16,24 +16,26 @@ int WakeOnLan::wake(MACAddress * macAddress)
 {
 		
 	uint8 ffBytes[6] = { 0xFF,0xFF,0xFF,0xFF,0xFF,0xFF };
-	const uint8 *macBytes = macAddress->getBytes();
-		
+	const uint8* macBytes = macAddress->getBytes();
+
 	const int packetSize = 6 + 16 * 6;
 	uint8 packet[packetSize];
-		
+
 	for (int i = 0; i < 6; ++i) packet[i] = ffBytes[i];
-		
+
 	for (int i = 0; i < 16; ++i)
 	{
-		for (int j = 0; j < 6; ++j)
+		for (int j = 0; j < 6; j++)
 		{
 			int index = 6 + i * 6 + j;
 			packet[index] = macBytes[j];
 		}
 	}
-		
-	DatagramSocket s;
-	return s.write(IPAddress::broadcast().toString(),9,packet,packetSize);
+
+	DatagramSocket s(true);
+	s.bindToPort(0);
+	return s.write(IPAddress::broadcast().toString(), 9, packet, packetSize);
+
 
 	//LOG("Wake ON Lan sent to " + macAddress->toString());
 }
