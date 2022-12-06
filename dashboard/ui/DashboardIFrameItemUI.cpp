c222d735653c2ca0dcca9906a877d82824f29d00
Ben Kuper <bkuperberg@gmail.com>
2022-12-06 23:04:17 +0100

    comment juce 7.0.3 code

--- dashboard/ui/DashboardIFrameItemUI.cpp
+++ dashboard/ui/DashboardIFrameItemUI.cpp
@@ -14,8 +14,8 @@ DashboardIFrameItemUI::DashboardIFrameItemUI(DashboardIFrameItem* item) :
 	DashboardItemUI(item),
 	iFrameItem(item)
 #if JUCE_WINDOWS 
-	,web(WebBrowserComponent::Options().withBackend(WebBrowserComponent::Options::Backend::webview2).withWinWebView2Options(
-		WebBrowserComponent::Options::WinWebView2().withUserDataFolder(File::getSpecialLocation(File::windowsLocalAppData).getChildFile("Chataigne")).withStatusBarDisabled()))
+	//,web(WebBrowserComponent::Options().withBackend(WebBrowserComponent::Options::Backend::webview2).withWinWebView2Options(
+	//	WebBrowserComponent::Options::WinWebView2().withUserDataFolder(File::getSpecialLocation(File::windowsLocalAppData).getChildFile("Chataigne")).withStatusBarDisabled())) //7.0.3
 #endif
 {
 
