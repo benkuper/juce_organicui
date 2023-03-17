14705636ac2011f27e55aae118149b1a9c4943b3
Ben Kuper <bkuperberg@gmail.com>
2023-03-17 21:03:10 +0100

    fix compilation errors

--- script/ScriptUtil.cpp
+++ script/ScriptUtil.cpp
@@ -191,7 +191,7 @@ var ScriptUtil::doubleToHexSeq(const var::NativeFunctionArgs& a)
 		oss << std::hex << std::uppercase << std::setfill('0') << std::setw(2) << i;
 	}
 
-	return oss.str();
+	return String(oss.str());
 }
 
 
