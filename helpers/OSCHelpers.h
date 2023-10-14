a249c4c09d3206922d9868582b4d3ae3294c5c15
Ben Kuper <bkuperberg@gmail.com>
2023-10-14 18:55:12 +0200

    custom oscqueryvaluecontainer create function

--- helpers/OSCHelpers.h
+++ helpers/OSCHelpers.h
@@ -60,14 +60,12 @@ public:
 class OSCQueryHelpers
 {
 public:
-	static void updateContainerFromData(ControllableContainer* cc, juce::var data, bool useAddressForNaming = false);
-	static void createOrUpdateControllableFromData(ControllableContainer* parentCC, Controllable* c, juce::StringRef name, juce::var data, bool useAddresForNaming = false);
 
 	class OSCQueryValueContainer :
-		public ControllableContainer
+		public EnablingControllableContainer
 	{
 	public:
-		OSCQueryValueContainer(const juce::String& name);
+		OSCQueryValueContainer(const juce::String& name, bool canListen = true, bool canSync = true);
 		~OSCQueryValueContainer();
 
 		BoolParameter* enableListen;
@@ -76,8 +74,14 @@ public:
 		InspectableEditor* getEditorInternal(bool isRoot, juce::Array<Inspectable*> inspectables = juce::Array<Inspectable*>()) override;
 	};
 
+	static void updateContainerFromData(ControllableContainer* cc, juce::var data, bool useAddressForNaming = false, std::function<OSCQueryValueContainer* (const juce::String& name)> createContainerFunc = nullptr);
+	
+	static void createOrUpdateControllableFromData(ControllableContainer* parentCC, Controllable* c, juce::StringRef name, juce::var data, bool useAddresForNaming = false);
+
+
+
 	class OSCQueryValueContainerEditor :
-		public GenericControllableContainerEditor
+		public EnablingControllableContainerEditor
 	{
 	public:
 		OSCQueryValueContainerEditor(OSCQueryValueContainer* cc, bool isRoot);
