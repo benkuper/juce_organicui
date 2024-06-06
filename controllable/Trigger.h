8903b85ffeccacc7fbaf8b2af22cc7610aee1f56
Ben Kuper <bkuperberg@gmail.com>
2024-06-06 09:28:08 +0200

    CriticalSection on triggers

--- controllable/Trigger.h
+++ controllable/Trigger.h
@@ -1,10 +1,10 @@
 /*
  ==============================================================================
- 
+
  Trigger.h
  Created: 8 Mar 2016 1:09:29pm
  Author:  bkupe
- 
+
  ==============================================================================
  */
 #pragma once
@@ -21,13 +21,13 @@ public:
 	~Trigger() { masterReference.clear(); }
 
 
-	TriggerButtonUI* createButtonUI(juce::Array<Trigger *> triggers = {});
-	TriggerImageUI* createImageUI(const juce::Image& image, bool keepSaturation = false, juce::Array<Trigger *> triggers = {});
-	TriggerBlinkUI * createBlinkUI(juce::Array<Trigger *> triggers = {});
-	ControllableUI * createDefaultUI(juce::Array<Controllable *> controllables = {}) override;
-	
+	TriggerButtonUI* createButtonUI(juce::Array<Trigger*> triggers = {});
+	TriggerImageUI* createImageUI(const juce::Image& image, bool keepSaturation = false, juce::Array<Trigger*> triggers = {});
+	TriggerBlinkUI* createBlinkUI(juce::Array<Trigger*> triggers = {});
+	ControllableUI* createDefaultUI(juce::Array<Controllable*> controllables = {}) override;
+
 	virtual void trigger();
-	
+
 	virtual DashboardItem* createDashboardItem() override;
 
 	// avoid feedback loop in listeners
@@ -39,28 +39,28 @@ public:
 	public:
 		/** Destructor. */
 		virtual ~Listener() {}
-		virtual void triggerTriggered(Trigger * p) = 0;
+		virtual void triggerTriggered(Trigger* p) = 0;
 	};
-	
-	juce::ListenerList<Listener> listeners;
+
+	juce::ListenerList<Listener, juce::Array<Listener*, juce::CriticalSection>> listeners;
 	void addTriggerListener(Trigger::Listener* newListener) { listeners.add(newListener); }
 	void removeTriggerListener(Trigger::Listener* listener) { listeners.remove(listener); }
-	
 
-	
+
+
 	QueuedNotifier<juce::WeakReference<Trigger>> queuedNotifier;
 	typedef QueuedNotifier<juce::WeakReference<Trigger>>::Listener AsyncListener;
-	void addAsyncTriggerListener(AsyncListener * l){queuedNotifier.addListener(l);}
-	void removeAsyncTriggerListener(AsyncListener * l){queuedNotifier.removeListener(l);}
-	
+	void addAsyncTriggerListener(AsyncListener* l) { queuedNotifier.addListener(l); }
+	void removeAsyncTriggerListener(AsyncListener* l) { queuedNotifier.removeListener(l); }
 
-	static Trigger * create() { return new Trigger("New Trigger",""); }
+
+	static Trigger* create() { return new Trigger("New Trigger", ""); }
 	virtual juce::String getTypeString() const override { return getTypeStringStatic(); }
 	static juce::String getTypeStringStatic() { return "Trigger"; }
 
 private:
 	juce::WeakReference<Trigger>::Master masterReference;
 	friend class juce::WeakReference<Trigger>;
-	
+
 	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(Trigger)
 };
