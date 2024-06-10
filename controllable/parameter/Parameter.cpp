37a55c0275294bdfd61abb40dcac80d8cb2aa16b
Norb <rostaing.norbert@gmail.com>
2024-06-10 16:48:47 +0200

    remove comment

--- controllable/parameter/Parameter.cpp
+++ controllable/parameter/Parameter.cpp
@@ -48,7 +48,6 @@ Parameter::Parameter(const Type& type, const String& niceName, const String& des
 
 Parameter::~Parameter()
 {
-	//isBeingDestroyed = true;
 	if (referenceTarget != nullptr) referenceTarget->removeParameterListener(this); //avoid reassigning on deletion
 	setReferenceParameter(nullptr);
 
