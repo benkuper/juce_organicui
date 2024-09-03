2a0651c8050749bfccde0dfaaa3949d825c7a25b
Ben Kuper <bkuperberg@gmail.com>
2024-09-03 17:58:54 -0400

    fix again

--- inspectable/Inspectable.h
+++ inspectable/Inspectable.h
@@ -17,7 +17,7 @@ class InspectableSelectionManager;
 #define COMMA ,
 #endif
 
-#define DECLARE_INSPECTACLE_LISTENER_BASE(ClassName, ListenerClass, listenerName, Section) ListenerClass ## <ClassName ## Listener Section> listenerName ## Listeners; \
+#define DECLARE_INSPECTACLE_LISTENER_BASE(ClassName, ListenerClass, listenerName, Section) ListenerClass<ClassName ## Listener Section> listenerName ## Listeners; \
 void add ## ClassName ## Listener(ClassName ## Listener* newListener) { listenerName ## Listeners.add(newListener); } \
 void remove ## ClassName ## Listener(ClassName ## Listener* listener) { if (isBeingDestroyed) return; listenerName ## Listeners.remove(listener); }
 
