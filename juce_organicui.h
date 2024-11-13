c9af6309f62bb8febd0b2bcf3d838bf96769eb71
Ben Kuper <bkuperberg@gmail.com>
2024-11-13 11:45:00 +0100

    add juce_javascript include

--- juce_organicui.h
+++ juce_organicui.h
@@ -119,6 +119,7 @@
 
 
 #include <juce_core/juce_core.h>
+#include <juce_javascript/juce_javascript.h>
 #include <juce_events/juce_events.h>
 #include <juce_gui_basics/juce_gui_basics.h>
 #include <juce_gui_extra/juce_gui_extra.h>
