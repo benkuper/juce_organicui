be34eeccbb31c19ccc06464ee3b8989e06271fba
Ben Kuper <bkuperberg@gmail.com>
2024-12-09 20:08:47 +0100

    force override if loading json data

--- controllable/parameter/Parameter.cpp
+++ controllable/parameter/Parameter.cpp
@@ -556,6 +556,8 @@ void Parameter::loadJSONDataInternal(var data)
 	}
 
 	alwaysNotify = data.getProperty("alwaysNotify", alwaysNotify);
+
+	isOverriden = true;
 }
 
 void Parameter::setupFromJSONData(var data)
