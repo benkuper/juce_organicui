df18de81a7c223b999e2d8eb80603ea64b071e56
Ben Kuper <bkuperberg@gmail.com>
2025-05-21 19:48:07 +0200

    fix mouse wheel mac

--- manager/ui/BaseManagerViewUI.h
+++ manager/ui/BaseManagerViewUI.h
@@ -247,7 +247,12 @@ void BaseManagerViewUI<M, T, U>::mouseWheelMove(const juce::MouseEvent& e, const
 		{
 			juce::Point<float> curMousePos = getViewMousePosition();
 
-			setViewZoom(this->manager->viewZoom + d.deltaY);
+			float delta = d.deltaY;
+#if JUCE_MAC
+			delta = d.deltaX;
+#endif
+
+			setViewZoom(this->manager->viewZoom + delta);
 
 			juce::Point<float> newMousePos = getViewMousePosition();
 			this->manager->viewOffset += ((newMousePos - curMousePos) * this->manager->viewZoom * (useCheckersAsUnits ? checkerSize : 1)).toInt();
